8ce31b78e320a0c00e81e4f679ecfae9c6fa750c
brntsllvn <brntsllvn@gmail.com>
2015-05-23 20:31:23 +0000

    migrate db

--- db/migrate/20150523201616_create_pieces.rb
+++ db/migrate/20150523201616_create_pieces.rb
@@ -1,10 +1,10 @@
 class CreatePieces < ActiveRecord::Migration
   def change
     create_table :pieces do |t|
-      t.string, :piece_type
-      t.string, :piece_color
-      t.integer, :x_position
-      t.integer, :y_position
+      t.string :piece_type
+      t.string :piece_color
+      t.integer :x_position
+      t.integer :y_position
       t.integer :game_id
 
       t.timestamps
--- db/migrate/20150523201801_create_games.rb
+++ db/migrate/20150523201801_create_games.rb
@@ -1,7 +1,7 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
-      t.integer, :white_user_id
+      t.integer :white_user_id
       t.integer :black_user_id
 
       t.timestamps
