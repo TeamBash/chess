ec62dede0be683d3a633eda36cdb13388c36477b
Seng Ngo <sengngo@gmail.com>
2015-06-05 04:46:24 +0000

    changed piece_type => type

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -16,11 +16,11 @@ class GameTest < ActiveSupport::TestCase
   	assert_equal 0, game.pieces.count
   	game.save
   	assert_equal 32, game.pieces.count
-		assert_equal 4, game.pieces.where(:piece_type => 'Rook').count
-    assert_equal 4, game.pieces.where(:piece_type => 'Knight').count
-    assert_equal 4, game.pieces.where(:piece_type => 'Bishop').count
-    assert_equal 2, game.pieces.where(:piece_type => 'Queen').count
-    assert_equal 2, game.pieces.where(:piece_type => 'King').count
-    assert_equal 16, game.pieces.where(:piece_type => 'Pawn').count
+		assert_equal 4, game.pieces.where(:type => 'Rook').count
+    assert_equal 4, game.pieces.where(:type => 'Knight').count
+    assert_equal 4, game.pieces.where(:type => 'Bishop').count
+    assert_equal 2, game.pieces.where(:type => 'Queen').count
+    assert_equal 2, game.pieces.where(:type => 'King').count
+    assert_equal 16, game.pieces.where(:type => 'Pawn').count
   end
 end
