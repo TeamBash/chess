549528f88b206571bcb9c8442e8154eb4afa5747
Seng Ngo <sengngo@gmail.com>
2015-06-05 04:41:56 +0000

    test_helper.rb change back.v2

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,15 @@
-ENV["RAILS_ENV"] ||= "test"
+ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  # Setup all fixtures in test/fixtures/*.yml
+  # for all tests in alphabetical order.
   #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+  # Note: You'll currently still have to declare fixtures
+  # explicitly in integration tests
   # -- they do not yet inherit this setting
   # fixtures :all
 
@@ -15,5 +17,5 @@ class ActiveSupport::TestCase
 end
 
 class ActionController::TestCase
-	include Devise::TestHelpers
+  include Devise::TestHelpers
 end
