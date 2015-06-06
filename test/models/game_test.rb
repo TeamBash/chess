11a39572d45389015c2ee4f288beccf949ab0eaa
hayli <haylic@gmail.com>
2015-06-06 17:20:25 +0000

    Updated unit test for game model

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -2,13 +2,11 @@ require 'test_helper'
 
 class GameTest < ActiveSupport::TestCase
   test 'game_created' do
-
     game = Game.create(name: 'Timezone Roulette', white_user_id: 1, black_user_id: 2)
-    game.pieces << King.new(color: 'black')
-    game.pieces << Queen.new(color: 'white')
-    expected = ['black', 'white', 'black', 'white']
-    actual = [game.pieces[0].color, game.pieces[1].color, game.pieces[2].color, game.pieces[3].color ]
+    expected = ['black', 'black', 'white', 'white']
+    actual = [game.pieces[0].color, game.pieces[8].color, game.pieces[16].color, game.pieces[24].color]
     assert_equal expected, actual
+    assert_equal 32, game.pieces.count
     assert_not_empty Game.all
   end
 
